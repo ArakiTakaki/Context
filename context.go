83dc5405cdcce47f50e1e6178f8a78844c598b79
ArakiTakaki <sjyyj008@gmail.com>
2018-03-16 12:21:58 +0900

    てst

--- context.go
+++ context.go
@@ -1,28 +1,37 @@
 package context
 
-var instance = make(map[string]interface{})
+type mapping struct {
+	instance map[string]interface{}
+}
+
+var context mapping
 
 // Put (key string, value mapping) 対応した構造体を返却する。
 func Put(key string, value interface{}) {
-	instance[key] = value
+	context.instance[key] = value
 }
 
 // Get 引数Key返り値mapping存在チェック
 func Get(key string) (interface{}, bool) {
-	work, swt := instance[key]
+	work, swt := context.instance[key]
 	return work, swt
+}
+
+// GetALL 全部の値を返却する
+func GetALL() mapping {
+	return context
 }
 
 // Len マップの長さを返却する
 func Len() int {
-	return len(instance)
+	return len(context.instance)
 }
 
 // Remove 値の削除
 func Remove(key string) bool {
-	_, swt := instance[key]
+	_, swt := context.instance[key]
 	if swt {
-		delete(instance, key)
+		delete(context.instance, key)
 	} else {
 		return false
 	}
